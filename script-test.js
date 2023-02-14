4eee9372d0eac42503d06373fd9a76cdfca65301
capitaozila <luis2001sousa@gmail.com>
2023-02-14 14:23:53 -0300

    edits

---
+++ script-test.js
@@ -0,0 +1,23 @@
+let searchButton = document.querySelector("#search")
+
+searchButton.addEventListener("click", ()=>{
+    sendApiRequest()
+})
+
+
+async function sendApiRequest(){
+    let API_KEY = "8700nxLyClJSqMNiE4xehxpNKRgfsTOP1oUhqS9o";
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
+    let data = await response.json()
+    console.log(data)
+    useApiData(data)
+}
+
+
+function useApiData(data){
+    // document.querySelector("#content").innerHTML += data.explanation
+    document.querySelector("#content").innerHTML += `<img src"${data.hdurl}">`
+    document.querySelector("#content").innerHTML += data.title
+    document.querySelector("#content").innerHTML += data.date
+}
+
